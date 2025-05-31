a35a67e6733d64d9b1f4d67135ef243d6f8e99da
Tisca Catalin <admin@tiscacatalin.com>
2025-06-01 01:23:26 +0200

    added missing files & renaming

---
+++ allofmyhomieslovezk/vlayer/src/hooks/useTwitterFollowersProof.ts
@@ -0,0 +1,138 @@
+import { useEffect, useState } from "react";
+import {
+  useCallProver,
+  useWaitForProvingResult,
+  useWebProof,
+  useChain,
+} from "@vlayer/react";
+import { useLocalStorage } from "usehooks-ts";
+import { WebProofConfig, ProveArgs } from "@vlayer/sdk";
+import { Abi, ContractFunctionName } from "viem";
+import {startPage, expectUrl, notarize, userAction} from "@vlayer/sdk/web_proof";
+import { UseChainError, WebProofError } from "../errors";
+import webProofProver from "../../../out/WebProofProver.sol/WebProofProver";
+
+export const useTwitterFollowersProof = (screenName: string) => {
+  const webProofConfig: WebProofConfig<Abi, string> = {
+    proverCallCommitment: {
+      address: "0x0000000000000000000000000000000000000000",
+      proverAbi: [],
+      functionName: "proveWeb",
+      commitmentArgs: [],
+      chainId: 1,
+    },
+    logoUrl: "http://twitterswap.com/logo.png",
+    steps: [
+      startPage("https://x.com/", "Go to x.com"),
+      expectUrl(`https://x.com/home`, `Navigate to your home page`),
+      userAction(
+          `https://x.com/${screenName}`,
+          `Navigate to your profile page`,
+          { text: `@${screenName}` },
+          { domElement: "body", require: { exist: true } }
+      ),
+      notarize(
+          `https://x.com/i/api/graphql/xWw45l6nX7DP2FKRyePXSw/UserByScreenName?variables=%7B%22screen_name%22%3A%22georgetisc84245%22%7D&features=%7B%22hidden_profile_subscriptions_enabled%22%3Atrue%2C%22profile_label_improvements_pcf_label_in_post_enabled%22%3Atrue%2C%22rweb_tipjar_consumption_enabled%22%3Atrue%2C%22verified_phone_label_enabled%22%3Afalse%2C%22subscriptions_verification_info_is_identity_verified_enabled%22%3Atrue%2C%22subscriptions_verification_info_verified_since_enabled%22%3Atrue%2C%22highlights_tweets_tab_ui_enabled%22%3Atrue%2C%22responsive_web_twitter_article_notes_tab_enabled%22%3Atrue%2C%22subscriptions_feature_can_gift_premium%22%3Atrue%2C%22creator_subscriptions_tweet_preview_api_enabled%22%3Atrue%2C%22responsive_web_graphql_skip_user_profile_image_extensions_enabled%22%3Afalse%2C%22responsive_web_graphql_timeline_navigation_enabled%22%3Atrue%7D&fieldToggles=%7B%22withAuxiliaryUserLabels%22%3Atrue%7D`,
+          "GET",
+          "Generate Proof of Twitter profile data",
+          [
+            {
+              request: {
+                headers_except: [],
+              },
+            },
+            {
+              response: {
+                headers_except: ["Transfer-Encoding"],
+              },
+            },
+          ],
+      )
+    ],
+  };
+
+  const [error, setError] = useState<Error | null>(null);
+
+  const {
+    requestWebProof,
+    webProof,
+    isPending: isWebProofPending,
+    error: webProofError,
+  } = useWebProof(webProofConfig);
+
+  if (webProofError) {
+    throw new WebProofError(webProofError.message);
+  }
+
+  const { chain, error: chainError } = useChain(
+    import.meta.env.VITE_CHAIN_NAME,
+  );
+  useEffect(() => {
+    if (chainError) {
+      setError(new UseChainError(chainError));
+    }
+  }, [chainError]);
+
+  const vlayerProverConfig: Omit<
+    ProveArgs<Abi, ContractFunctionName<Abi>>,
+    "args"
+  > = {
+    address: import.meta.env.VITE_PROVER_ADDRESS as `0x${string}`,
+    proverAbi: webProofProver.abi,
+    chainId: chain?.id,
+    functionName: "followersProof",
+    gasLimit: Number(import.meta.env.VITE_GAS_LIMIT),
+  };
+
+  const {
+    callProver,
+    isPending: isCallProverPending,
+    isIdle: isCallProverIdle,
+    data: hash,
+    error: callProverError,
+  } = useCallProver(vlayerProverConfig);
+
+  if (callProverError) {
+    throw callProverError;
+  }
+
+  const {
+    isPending: isWaitingForProvingResult,
+    data: result,
+    error: waitForProvingResultError,
+  } = useWaitForProvingResult(hash);
+
+  if (waitForProvingResultError) {
+    throw waitForProvingResultError;
+  }
+
+  const [, setWebProof] = useLocalStorage("webProof", "");
+  const [, setProverResult] = useLocalStorage("proverResult", "");
+
+  useEffect(() => {
+    if (webProof) {
+      console.log("webProof", webProof);
+      setWebProof(JSON.stringify(webProof));
+    }
+  }, [JSON.stringify(webProof)]);
+
+  useEffect(() => {
+    if (result) {
+      console.log("proverResult", result);
+      setProverResult(JSON.stringify(result));
+    }
+  }, [JSON.stringify(result)]);
+
+  return {
+    requestWebProof,
+    webProof,
+    isPending:
+      isWebProofPending || isCallProverPending || isWaitingForProvingResult,
+    isCallProverIdle,
+    isWaitingForProvingResult,
+    isWebProofPending,
+    callProver,
+    result,
+    error,
+  };
+};
